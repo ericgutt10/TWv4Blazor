10483233ecaa5fc3155674efbf0b91c54a4db7ca
ericguttbruno <eric.guttormson@bruno.com>
2025-03-30 10:53:03 -0500

    Initial Commit

---
+++ Host/HostAssy.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace Host;
+
+public static class HostAssy
+{
+    private static Assembly[]? _assemblies;
+
+    public static Assembly[] Assemblies => _assemblies ??= [typeof(App.Components.Layout.MainLayout).Assembly];
+}
